0c4bd6fe339ce8c0e3aa5a588e63dcf119bbb4eb
lizhongze <lizhongze1108@foxmail.com>
2019-10-22 16:58:57 +0800

    优化：gulp 自动部署

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const GulpSSH = require('gulp-ssh');
 // const isProduct = APP_ENV === 'production';
 
 // 需要上传到服务器的路径
-const remotePath = '/src/*';
+const remotePath = '/src/';
 // const remotePath = `/home/public/docker/main/${npm_package_name}`;
 
 const config = {
@@ -31,7 +31,8 @@ gulp.task('execSSH', done => {
     console.log('删除服务器上现有文件...');
     // return gulpSSH.shell(config.commands, { filePath: 'commands.log' }).pipe(gulp.dest('logs'));
     // gulpSSH.shell(config.commands, { filePath: 'commands.log' }).pipe(gulp.dest('logs'));
-    gulpSSH.shell(config.commands);
+    gulpSSH.shell('rm /src', { filePath: 'commands.log' }).pipe(gulp.dest('logs'));
+    // gulpSSH.shell(`${config.commands}index.html`);
     done();
 });
 
@@ -42,10 +43,15 @@ gulp.task(
     'deploy',
     gulp.series('execSSH', done => {
         console.log('2s后开始上传文件到服务器...');
-        setTimeout(() => {
-            gulp.src(`./dist/*`).pipe(gulpSSH.dest(config.path));
-            console.log('上传完毕.....');
-            done();
-        }, 2000);
+        // setTimeout(() => {
+        //     gulp.src(['./dist/*']).pipe(gulpSSH.dest(config.path));
+        //     gulp.src(['./dist/css/*']).pipe(gulpSSH.dest(config.path + 'css'));
+        //     gulp.src(['./dist/fonts/*']).pipe(gulpSSH.dest(config.path + 'font'));
+        //     gulp.src(['./dist/img/*']).pipe(gulpSSH.dest(config.path + 'img'));
+        //     gulp.src(['./dist/js/*']).pipe(gulpSSH.dest(config.path + 'js'));
+        //     console.log('上传完毕.....');
+        //     done();
+        // }, 2000);
+        done();
     })
 );
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
     configureWebpack: {
         devtool: 'source-map'
+    },
+    pluginOptions: {
+        ssh: {
+            projectName: 'my-blog',
+            host: '39.105.144.5',
+            port: 4000,
+            username: 'hobbs',
+            password: 'sftp123456',
+            remotePath: '/src/',
+            localPath: 'dist'
+        },
+        ftpdeploy: {
+            localBasePath: '/',
+            remoteBasePath: '/src/',
+            clear: ["src"],
+            sync: [{ src: '/dist', dest: '/' }]
+        }
     }
-}
+};
